6daf4f568004bf64244993927d9d7c1bd13a62ab
ruslana <ruslana.prus@ukr.net>
2024-11-24 12:38:44 +0100

    add INoteRepository

--- src/main/java/com/example/notemanager/service/NoteService.java
+++ src/main/java/com/example/notemanager/service/NoteService.java
@@ -1,31 +1,30 @@
 package com.example.notemanager.service;
 
-import com.example.notemanager.exception.NoteNotFoundException;
 import com.example.notemanager.model.Note;
+import com.example.notemanager.repository.INoteRepository;
 import com.example.notemanager.util.IdGeneratorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 public class NoteService implements INoteService {
-    private final Map<Long, Note> notes;
+    private final INoteRepository noteRepository;
     private final IdGeneratorService idGeneratorService;
 
     @GetMapping
     public List<Note> listAll() {
-        return new ArrayList<>(notes.values());
+        return noteRepository.findAll();
     }
 
     @Override
     public Note getById(long id) {
-        return notes.get(id);
+        return noteRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Note not found"));
     }
 
     @Override
@@ -36,29 +35,24 @@ public class NoteService implements INoteService {
                 .title(note.getTitle())
                 .content(note.getContent())
                 .build();
-        notes.put(id, newNote);
+        noteRepository.save(newNote);
         return newNote;
     }
 
     @Override
     public Note update(Note note) {
-        return Optional.ofNullable(notes.get(note.getId()))
-                .map(existingNote -> {
-                    Note updatedNote = Note.builder()
-                            .id(note.getId())
-                            .title(note.getTitle())
-                            .content(note.getContent())
-                            .build();
-                    notes.put(note.getId(), updatedNote);
-                    return updatedNote;
-                })
-                .orElseThrow(() -> new NoteNotFoundException("Note with id " + note.getId() + " not found"));
+        if(!noteRepository.existsById(note.getId())) {
+            throw new NoSuchElementException("Note does not exist");
+        }
+        noteRepository.save(note);
+        return note;
     }
 
     @Override
     public void delete(long id) {
-        if (notes.remove(id) == null) {
-            throw new NoteNotFoundException("Note with id " + id + " not found");
+        if(!noteRepository.existsById(id)) {
+            throw new NoSuchElementException("Note does not exist");
         }
+        noteRepository.deleteById(id);
     }
 }
